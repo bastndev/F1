ca37fa64d57c003826f203569d9ceff6da3d0fde
bastndev <gohit24@gmail.com>
2025-09-04 17:44:31 -0500

    🏗️ [Update] remove keymapas.ts and refactor F1TreeProvider in extension.ts for improved structure

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,17 @@
 import * as vscode from 'vscode';
 
-class F1TreeDataProvider implements vscode.TreeDataProvider<string> {
+class F1TreeProvider implements vscode.TreeDataProvider<string> {
     getTreeItem(element: string): vscode.TreeItem {
-        const item = new vscode.TreeItem(element);
-        item.tooltip = `Hello World item: ${element}`;
-        return item;
+        return new vscode.TreeItem(element);
     }
-
-    getChildren(element?: string): string[] {
-        if (!element) {
-            return ['Hello World', 'F1 Extension', 'Working!'];
-        }
-        return [];
+    
+    getChildren(): string[] {
+        return ['Hello Wold 🧪'];
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    // Registrar TreeDataProvider
-    const treeDataProvider = new F1TreeDataProvider();
-    vscode.window.registerTreeDataProvider('f1-shortcuts', treeDataProvider);
-
-    // Command to open panel
-    let openPanelCommand = vscode.commands.registerCommand('f1.openPanel', () => {
-        vscode.window.showInformationMessage('Hello World from F1 Extension!');
-    });
-
-    // Command to toggle panel
-    let togglePanelCommand = vscode.commands.registerCommand('f1.togglePanel', () => {
-        vscode.window.showInformationMessage('Hello World - Toggle F1 Panel!');
-    });
-
-    context.subscriptions.push(openPanelCommand);
-    context.subscriptions.push(togglePanelCommand);
+    vscode.window.registerTreeDataProvider('f1-shortcuts', new F1TreeProvider());
 }
 
 export function deactivate() {}
