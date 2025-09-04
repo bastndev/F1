ccb80217b6646f26c9374944cb558e9fc2a27bda
bastndev <gohit24@gmail.com>
2025-09-04 18:08:21 -0500

    🏗️ [Update] refactor F1TreeProvider to ExtensionTreeProvider for improved structure and type safety

--- src/disable-enable/extensions/extensions.ts
+++ src/disable-enable/extensions/extensions.ts
@@ -1,17 +1,33 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-class F1TreeProvider implements vscode.TreeDataProvider<string> {
-    getTreeItem(element: string): vscode.TreeItem {
-        return new vscode.TreeItem(element);
+interface ExtensionItem {
+    name: string;
+    iconPath?: string;
+}
+
+class ExtensionTreeProvider implements vscode.TreeDataProvider<ExtensionItem> {
+    getTreeItem(element: ExtensionItem): vscode.TreeItem {
+        const item = new vscode.TreeItem(element.name);
+        item.iconPath = element.iconPath;
+        return item;
     }
-    
-    getChildren(): string[] {
-        return ['Hello Wold - EXTENSION 🧪'];
+
+    getChildren(): ExtensionItem[] {
+        return vscode.extensions.all
+            .filter(ext => !ext.packageJSON.isBuiltin)
+            .map(ext => ({
+                name: ext.packageJSON.displayName || ext.packageJSON.name,
+                iconPath: ext.packageJSON.icon 
+                    ? path.join(ext.extensionPath, ext.packageJSON.icon) 
+                    : undefined
+            }))
+            .sort((a, b) => a.name.localeCompare(b.name));
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    vscode.window.registerTreeDataProvider('f1-extensions', new F1TreeProvider());
+    vscode.window.registerTreeDataProvider('f1-extensions', new ExtensionTreeProvider());
 }
 
 export function deactivate() {}
