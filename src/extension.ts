fda34bd971f0231711d87c065d88aa9d98f8e4cc
bastndev <gohit24@gmail.com>
2025-09-07 17:52:46 -0500

    🏗️ [Add] implement tree data providers for editor controls and extensions; update activation logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
-import { activate as activateShortcuts } from './disable-enable/shortcuts/shortcuts';
+import { activate as activateShortcuts } from './disable-enable/shortcuts/my-list';
 import { activate as activateEditorControls } from './disable-enable/editor-controls/editor-controls';
 import { activate as activateExtensions } from './disable-enable/extensions/extensions';
-import { activate as activateKeymaps } from './disable-enable/default-keymaps';
+import { activate as activateKeymaps } from './disable-enable/shortcuts/default-keymaps';
 
 export function activate(context: vscode.ExtensionContext) {
     // Activate each sub-module to register their tree data providers
