5571a1b39a1db85ffb82f1ed104ea1bcb531c875
bastndev <gohit24@gmail.com>
2025-09-07 17:41:47 -0500

    🏗️ [Add] implement AI suggestions toggle functionality with enhanced notification and state checking

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { activate as activateShortcuts } from './disable-enable/shortcuts/shortcuts';
 import { activate as activateEditorControls } from './disable-enable/editor-controls/editor-controls';
 import { activate as activateExtensions } from './disable-enable/extensions/extensions';
-import { activate as activateKeymaps } from '../src/disable-enable/keymaps';
+import { activate as activateKeymaps } from './disable-enable/default-keymaps';
 
 export function activate(context: vscode.ExtensionContext) {
     // Activate each sub-module to register their tree data providers
