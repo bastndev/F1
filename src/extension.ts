e8fa2577ad47a5401a2d6e4b3c9f6dd6244b7771
gohitx <gohit21@gmail.com>
2026-05-27 22:22:45 -0500

    🏗️ feat: implement CLI Hub webview panel and provider registration

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CliHubViewProvider } from './cli/index';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,6 +10,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "2" is now active!');
+
+	const cliHubProvider = new CliHubViewProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(CliHubViewProvider.viewType, cliHubProvider)
+	);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
